de4a03e2c1768f465f4db0ef30f90f264289ef5c
Benjamin Sampson <bensampsondev@gmail.com>
2019-01-09 17:24:20 +1100

    Add Google Site HTML tag verification

--- pages/_document.js
+++ pages/_document.js
@@ -28,6 +28,8 @@ class MyDocument extends Document {
           <link rel="stylesheet" href="https://d1azc1qln24ryf.cloudfront.net/114779/Socicon/style-cf.css?9ukd8d" />
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 
+          <meta name="google-site-verification" content="shNujxkz6dsoVYeJS2M0tWiMm6ufb9nbDnF35nc5a78" />
+
           {this.props.styleTags}
         </Head>
         <body>
@@ -35,24 +37,18 @@ class MyDocument extends Document {
           <NextScript />
 
           {/* GOOGLE ANALYTICS */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127404055-4"></script>
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127404055-4" />
+          <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
 
                 gtag('config', 'UA-127404055-4');
-              `
-            }} />
+              ` }} />
           {/* END GOOGLE ANALYTICS */}
 
           {/* HOTJAR TRACKING CODE */}
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{ __html: `
+          <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
               (function(h,o,t,j,a,r){
                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
                 h._hjSettings={hjid:1146797,hjsv:6};
@@ -61,8 +57,7 @@ class MyDocument extends Document {
                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                 a.appendChild(r);
             })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `}}
-          />
+            ` }} />
           {/* END HOTJAR TRACKING CODE */}
 
           {/* DRIFT */}
@@ -92,7 +87,7 @@ class MyDocument extends Document {
 
             drift.SNIPPET_VERSION = '0.3.1';
             drift.load('6kz9xma6ds4d');
-          `}} />
+          ` }} />
           {/* END DRIFT */}
         </body>
       </html>
